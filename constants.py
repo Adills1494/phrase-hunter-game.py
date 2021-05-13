483b25356a665c123142de4f1d9c85cd06dce8ad
Adills1494 <79712908+Adills1494@users.noreply.github.com>
2021-05-12 20:40:11 -0400

    Update constants.py

--- constants.py
+++ constants.py
@@ -1,1 +1,9 @@
+PHRASES = [
+    ("Doth mother know you weareth her drapes", "Shakespeare in the park? More like Tony Stark in the park")
+    ("You gonna eat your tots", "In the cafeteria with Nepoleon Dynomite")
+    ("With great power comes great responsibility", "Bitten by a radioactive spider and this is what you tell me uncle?")
+    ("Honey Where is my super suit", "When your wife hides your suit and you have to save the city")
+    ("Why so serious", "So let me tell you about my father Batman")
+]
+
 
